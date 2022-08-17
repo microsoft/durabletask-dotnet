33843a39aa9603e3d9e0f37fe99a53e594de7eae
Jacob Viau <javia@microsoft.com>
2022-08-17 13:53:03 -0700

    Suppress build warnings

--- src/DurableTask/Logs.cs
+++ src/DurableTask/Logs.cs
@@ -49,15 +49,21 @@ namespace Microsoft.DurableTask
         public static partial void WorkerBusy(this ILogger logger);
 
         [LoggerMessage(EventId = 40, Level = LogLevel.Information, Message = "Scheduling new {name} orchestration with instance ID '{instanceId}' and {sizeInBytes} bytes of input data.")]
+#pragma warning disable SYSLIB1015 // Argument is not referenced from the logging message
         public static partial void SchedulingOrchestration(this ILogger logger, string instanceId, string name, int sizeInBytes, DateTimeOffset startTime);
+#pragma warning restore SYSLIB1015 // Argument is not referenced from the logging message
 
         // NOTE: fetchInputsAndOutputs seems like something that could be left out of the text message and just included in the structured logs
         [LoggerMessage(EventId = 42, Level = LogLevel.Information, Message = "Waiting for instance '{instanceId}' to start.")]
+#pragma warning disable SYSLIB1015 // Argument is not referenced from the logging message
         public static partial void WaitingForInstanceStart(this ILogger logger, string instanceId, bool fetchInputsAndOutputs);
+#pragma warning restore SYSLIB1015 // Argument is not referenced from the logging message
 
         // NOTE: fetchInputsAndOutputs seems like something that could be left out of the text message and just included in the structured logs
         [LoggerMessage(EventId = 43, Level = LogLevel.Information, Message = "Waiting for instance '{instanceId}' to complete, fail, or terminate.")]
+#pragma warning disable SYSLIB1015 // Argument is not referenced from the logging message
         public static partial void WaitingForInstanceCompletion(this ILogger logger, string instanceId, bool fetchInputsAndOutputs);
+#pragma warning restore SYSLIB1015 // Argument is not referenced from the logging message
 
         [LoggerMessage(EventId = 44, Level = LogLevel.Information, Message = "Terminating instance '{instanceId}'.")]
         public static partial void TerminatingInstance(this ILogger logger, string instanceId);

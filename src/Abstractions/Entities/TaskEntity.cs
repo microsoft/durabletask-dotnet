16a5d04c2c64baa38e9f5449cfdd7ac747a39d62
Jacob Viau <javia@microsoft.com>
2023-08-08 15:24:54 -0700

    Add method dispatching TaskEntity base class (#171)

--- src/Abstractions/Entities/TaskEntity.cs
+++ src/Abstractions/Entities/TaskEntity.cs
@@ -1,11 +1,22 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Reflection;
+using System.Threading.Tasks;
+
 namespace Microsoft.DurableTask.Entities;
 
 /// <summary>
 /// The task entity contract.
 /// </summary>
+/// <remarks>
+/// <para><b>Entity State</b></para>
+/// <para>
+/// All entity implementations are required to be serializable by the configured <see cref="DataConverter"/>. An entity
+/// will have its state deserialized before executing an operation, and then the new state will be the serialized value
+/// of the <see cref="ITaskEntity"/> implementation instance post-operation.
+/// </para>
+/// </remarks>
 public interface ITaskEntity
 {
     /// <summary>
@@ -14,4 +25,222 @@ public interface ITaskEntity
     /// <param name="operation">The operation to run.</param>
     /// <returns>The response to the caller, if any.</returns>
     ValueTask<object?> RunAsync(TaskEntityOperation operation);
+}
+
+/// <summary>
+/// An <see cref="ITaskEntity"/> which dispatches its operations to public instance methods or properties.
+/// </summary>
+/// <remarks>
+/// <para><b>Method Binding</b></para>
+/// <para>
+/// When using this base class, all public methods will be considered valid entity operations.
+/// <list type="bullet">
+/// <item>Only public methods are considered (private, internal, and protected are not.)</item>
+/// <item>Properties are not considered.</item>
+/// <item>Operation matching is case insensitive.</item>
+/// <item><see cref="NotSupportedException"/> is thrown if no matching public method is found for an operation.</item>
+/// <item><see cref="AmbiguousMatchException"/> is thrown if there are multiple public overloads for an operation name.</item>
+/// </list>
+/// </para>
+///
+/// <para><b>Parameter Binding</b></para>
+/// <para>
+/// Operation methods supports parameter binding as follows:
+/// <list type="bullet">
+/// <item>Can bind to the context by adding a parameter of type <see cref="TaskEntityContext"/>.</item>
+/// <item>Can bind to the raw operation by adding a parameter of type <see cref="TaskEntityOperation"/>.</item>
+/// <item>Can bind to the operation input directly by adding any parameter which does not match a previously described
+/// binding candidate. The operation input, if available, will be deserialized to that type.</item>
+/// <item>Default parameters can be used for input to allow for an operation to execute (with the default value) without
+/// an input being provided.</item>
+/// </list>
+///
+/// <see cref="InvalidOperationException" /> will be thrown if:
+/// <list type="bullet">
+/// <item>There is a redundant parameter binding (ie: two context, operation, or input matches)</item>
+/// <item>There is an input binding, but no input was provided.</item>
+/// <item>There is another unknown type present which does not match context, operation, or input.</item>
+/// </list>
+/// </para>
+///
+/// <para><b>Return Value</b></para>
+/// <para>
+/// Any value returned by the bound method will be returned to the operation caller. Not all callers wait for a return
+/// value, such as signal-only callers. The return value is ignored in these cases.
+/// </para>
+///
+/// <para><b>Entity State</b></para>
+/// <para>
+/// Unchanged from <see cref="ITaskEntity"/>. Entity state is the serialized value of the entity after an operation
+/// completes.
+/// </para>
+/// </remarks>
+public abstract class TaskEntity : ITaskEntity
+{
+    /**
+     * TODO:
+     * 1. Consider caching a compiled delegate for a given operation name.
+     */
+    static readonly BindingFlags InstanceBindingFlags
+            = BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase;
+
+    /// <inheritdoc/>
+    public ValueTask<object?> RunAsync(TaskEntityOperation operation)
+    {
+        Check.NotNull(operation);
+        if (!this.TryDispatchMethod(operation, out object? result, out Type returnType))
+        {
+            throw new NotSupportedException($"No suitable method found for entity operation '{operation}'.");
+        }
+
+        if (typeof(Task).IsAssignableFrom(returnType))
+        {
+            // Task or Task<T>
+            return new(AsGeneric((Task)result!, returnType)); // we assume a declared Task return type is never null.
+        }
+
+        if (returnType == typeof(ValueTask))
+        {
+            // ValueTask
+            return AsGeneric((ValueTask)result!); // we assume a declared ValueTask return type is never null.
+        }
+
+        if (returnType.IsGenericType && returnType.GetGenericTypeDefinition() == typeof(ValueTask<>))
+        {
+            // ValueTask<T>
+            return AsGeneric(result!, returnType); // No inheritance, have to do purely via reflection.
+        }
+
+        return new(result);
+    }
+
+    static bool TryGetInput(ParameterInfo parameter, TaskEntityOperation operation, out object? input)
+    {
+        if (!operation.HasInput)
+        {
+            if (parameter.HasDefaultValue)
+            {
+                input = parameter.DefaultValue;
+                return true;
+            }
+
+            input = null;
+            return false;
+        }
+
+        input = operation.GetInput(parameter.ParameterType);
+        return true;
+    }
+
+    static async Task<object?> AsGeneric(Task task, Type declared)
+    {
+        await task;
+        if (declared.IsGenericType && declared.GetGenericTypeDefinition() == typeof(Task<>))
+        {
+            return declared.GetProperty("Result", BindingFlags.Public | BindingFlags.Instance).GetValue(task);
+        }
+
+        return null;
+    }
+
+    static ValueTask<object?> AsGeneric(ValueTask t)
+    {
+        static async Task<object?> Await(ValueTask t)
+        {
+            await t;
+            return null;
+        }
+
+        if (t.IsCompletedSuccessfully)
+        {
+            return default;
+        }
+
+        return new(Await(t));
+    }
+
+    static ValueTask<object?> AsGeneric(object result, Type type)
+    {
+        // result and type here must be some form of ValueTask<T>.
+        if ((bool)type.GetProperty("IsCompletedSuccessfully").GetValue(result))
+        {
+            return new(type.GetProperty("Result").GetValue(result));
+        }
+        else
+        {
+            Task t = (Task)type.GetMethod("AsTask", BindingFlags.Instance | BindingFlags.Public)
+                .Invoke(result, null);
+            return new(t.ToGeneric<object?>());
+        }
+    }
+
+    bool TryDispatchMethod(TaskEntityOperation operation, out object? result, out Type returnType)
+    {
+        Type t = this.GetType();
+
+        // Will throw AmbiguousMatchException if more than 1 overload for the method name exists.
+        MethodInfo? method = t.GetMethod(operation.Name, InstanceBindingFlags);
+        if (method is null)
+        {
+            result = null;
+            returnType = typeof(void);
+            return false;
+        }
+
+        ParameterInfo[] parameters = method.GetParameters();
+        object?[] inputs = new object[parameters.Length];
+
+        int i = 0;
+        ParameterInfo? inputResolved = null;
+        ParameterInfo? contextResolved = null;
+        ParameterInfo? operationResolved = null;
+        foreach (ParameterInfo parameter in parameters)
+        {
+            if (parameter.ParameterType == typeof(TaskEntityContext))
+            {
+                ThrowIfDuplicateBinding(contextResolved, parameter, "context", operation);
+                inputs[i] = operation.Context;
+                contextResolved = parameter;
+            }
+            else if (parameter.ParameterType == typeof(TaskEntityOperation))
+            {
+                ThrowIfDuplicateBinding(operationResolved, parameter, "operation", operation);
+                inputs[i] = operation;
+                operationResolved = parameter;
+            }
+            else
+            {
+                ThrowIfDuplicateBinding(inputResolved, parameter, "input", operation);
+                if (TryGetInput(parameter, operation, out object? input))
+                {
+                    inputs[i] = input;
+                    inputResolved = parameter;
+                }
+                else
+                {
+                    throw new InvalidOperationException($"Error dispatching {operation} to '{method}'.\n" +
+                        $"There was an error binding parameter '{parameter}'. The operation expected an input value, " +
+                        "but no input was provided by the caller.");
+                }
+            }
+
+            i++;
+        }
+
+        result = method.Invoke(this, inputs);
+        returnType = method.ReturnType;
+        return true;
+
+        static void ThrowIfDuplicateBinding(
+            ParameterInfo? existing, ParameterInfo parameter, string bindingConcept, TaskEntityOperation operation)
+        {
+            if (existing is not null)
+            {
+                throw new InvalidOperationException($"Error dispatching {operation} to '{parameter.Member}'.\n" +
+                    $"Unable to bind {bindingConcept} to '{parameter}' because it has " +
+                    $"already been bound to parameter '{existing}'. Please remove the duplicate parameter in method " +
+                    $"'{parameter.Member}'.\nEntity operation: {operation}.");
+            }
+        }
+    }
 }
---
+++ test/Abstractions.Tests/Entities/TaskEntityTests.cs
@@ -0,0 +1,299 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Reflection;
+using DotNext;
+
+namespace Microsoft.DurableTask.Entities.Tests;
+
+public class TaskEntityTests
+{
+    [Theory]
+    [InlineData("doesNotExist")] // method does not exist.
+    [InlineData("add")] // private method, should not work.
+    [InlineData("staticMethod")] // public static methods are not supported.
+    public async Task OperationNotSupported_Fails(string name)
+    {
+        Operation operation = new(name, Mock.Of<TaskEntityContext>(), 10);
+        TestEntity entity = new();
+
+        Func<Task<object?>> action = () => entity.RunAsync(operation).AsTask();
+
+        await action.Should().ThrowAsync<NotSupportedException>();
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public async Task TaskOperation_Success(
+        [CombinatorialValues("TaskOp", "TaskOfStringOp", "ValueTaskOp", "ValueTaskOfStringOp")] string name, bool sync)
+    {
+        object? expected = name.Contains("OfString") ? "success" : null;
+        Operation operation = new(name, Mock.Of<TaskEntityContext>(), sync);
+        TestEntity entity = new();
+
+        object? result = await entity.RunAsync(operation);
+
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public async Task Add_Success([CombinatorialRange(0, 14)] int method, bool lowercase)
+    {
+        int start = Random.Shared.Next(0, 10);
+        int toAdd = Random.Shared.Next(0, 10);
+        string opName = lowercase ? "add" : "Add";
+        Operation operation = new($"{opName}{method}", Mock.Of<TaskEntityContext>(), toAdd);
+        TestEntity entity = new() { Value = start };
+
+        object? result = await entity.RunAsync(operation);
+
+        int expected = start + toAdd;
+        entity.Value.Should().Be(expected);
+        result.Should().BeOfType<int>().Which.Should().Be(expected);
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public async Task Get_Success([CombinatorialRange(0, 2)] int method, bool lowercase)
+    {
+        int expected = Random.Shared.Next(0, 10);
+        string opName = lowercase ? "get" : "Get";
+        Operation operation = new($"{opName}{method}", Mock.Of<TaskEntityContext>(), default);
+        TestEntity entity = new() { Value = expected };
+
+        object? result = await entity.RunAsync(operation);
+
+        entity.Value.Should().Be(expected);
+        result.Should().BeOfType<int>().Which.Should().Be(expected);
+    }
+
+    [Fact]
+    public async Task Add_NoInput_Fails()
+    {
+        Operation operation = new("add0", Mock.Of<TaskEntityContext>(), default);
+        TestEntity entity = new();
+
+        Func<Task<object?>> action = () => entity.RunAsync(operation).AsTask();
+
+        await action.Should().ThrowAsync<InvalidOperationException>();
+    }
+
+    [Theory]
+    [CombinatorialData]
+    public async Task Dispatch_AmbiguousArgs_Fails([CombinatorialRange(0, 3)] int method)
+    {
+        Operation operation = new($"ambiguousArgs{method}", Mock.Of<TaskEntityContext>(), 10);
+        TestEntity entity = new();
+
+        Func<Task<object?>> action = () => entity.RunAsync(operation).AsTask();
+
+        await action.Should().ThrowAsync<InvalidOperationException>();
+    }
+
+    [Fact]
+    public async Task Dispatch_AmbiguousMatch_Fails()
+    {
+        Operation operation = new("ambiguousMatch", Mock.Of<TaskEntityContext>(), 10);
+        TestEntity entity = new();
+
+        Func<Task<object?>> action = () => entity.RunAsync(operation).AsTask();
+        await action.Should().ThrowAsync<AmbiguousMatchException>();
+    }
+
+    [Fact]
+    public async Task DefaultValue_NoInput_Succeeds()
+    {
+        Operation operation = new("defaultValue", Mock.Of<TaskEntityContext>(), default);
+        TestEntity entity = new();
+
+        object? result = await entity.RunAsync(operation);
+
+        result.Should().BeOfType<string>().Which.Should().Be("default");
+    }
+
+    [Fact]
+    public async Task DefaultValue_Input_Succeeds()
+    {
+        Operation operation = new("defaultValue", Mock.Of<TaskEntityContext>(), "not-default");
+        TestEntity entity = new();
+
+        object? result = await entity.RunAsync(operation);
+
+        result.Should().BeOfType<string>().Which.Should().Be("not-default");
+    }
+
+    class Operation : TaskEntityOperation
+    {
+        readonly Optional<object?> input;
+
+        public Operation(string name, TaskEntityContext context, Optional<object?> input)
+        {
+            this.Name = name;
+            this.Context = context;
+            this.input = input;
+        }
+
+        public override string Name { get; }
+
+        public override TaskEntityContext Context { get; }
+
+        public override bool HasInput => this.input.IsPresent;
+
+        public override object? GetInput(Type inputType)
+        {
+            if (!this.input.IsPresent)
+            {
+                throw new InvalidOperationException("No input available.");
+            }
+
+            if (this.input.Value is null)
+            {
+                return null;
+            }
+
+            if (!inputType.IsAssignableFrom(this.input.Value.GetType()))
+            {
+                throw new InvalidCastException("Cannot convert input type.");
+            }
+
+            return this.input.Value;
+        }
+    }
+
+    class TestEntity : TaskEntity
+    {
+        public int Value { get; set; }
+
+        public static string StaticMethod() => throw new NotImplementedException();
+
+        // All possible permutations of the 3 inputs we support: object, context, operation
+        // 14 via Add, 2 via Get: 16 total.
+        public int Add0(int value) => this.Add(value, default, default);
+
+        public int Add1(int value, TaskEntityContext context) => this.Add(value, context, default);
+
+        public int Add2(int value, TaskEntityOperation operation) => this.Add(value, default, operation);
+
+        public int Add3(int value, TaskEntityContext context, TaskEntityOperation operation)
+            => this.Add(value, context, operation);
+
+        public int Add4(int value, TaskEntityOperation operation, TaskEntityContext context)
+            => this.Add(value, context, operation);
+
+        public int Add5(TaskEntityOperation operation) => this.Add(default, default, operation);
+
+        public int Add6(TaskEntityOperation operation, int value) => this.Add(value, default, operation);
+
+        public int Add7(TaskEntityOperation operation, TaskEntityContext context)
+            => this.Add(default, context, operation);
+
+        public int Add8(TaskEntityOperation operation, int value, TaskEntityContext context)
+            => this.Add(value, context, operation);
+
+        public int Add9(TaskEntityOperation operation, TaskEntityContext context, int value)
+            => this.Add(value, context, operation);
+
+        public int Add10(TaskEntityContext context, int value)
+            => this.Add(value, context, default);
+
+        public int Add11(TaskEntityContext context, TaskEntityOperation operation)
+            => this.Add(default, context, operation);
+
+        public int Add12(TaskEntityContext context, int value, TaskEntityOperation operation)
+            => this.Add(value, context, operation);
+
+        public int Add13(TaskEntityContext context, TaskEntityOperation operation, int value)
+            => this.Add(value, context, operation);
+
+        public int Get0() => this.Get(default);
+
+        public int Get1(TaskEntityContext context) => this.Get(context);
+
+        public int AmbiguousMatch(TaskEntityContext context) => this.Value;
+
+        public int AmbiguousMatch(TaskEntityOperation operation) => this.Value;
+
+        public int AmbiguousArgs0(int value, object other) => this.Add0(value);
+
+        public int AmbiguousArgs1(int value, TaskEntityContext context, TaskEntityContext context2) => this.Add0(value);
+
+        public int AmbiguousArgs2(int value, TaskEntityOperation operation, TaskEntityOperation operation2)
+            => this.Add0(value);
+
+        public string DefaultValue(string toReturn = "default") => toReturn;
+
+        public Task TaskOp(bool sync)
+        {
+            static async Task Slow()
+            {
+                await Task.Yield();
+            }
+
+            return sync ? Task.CompletedTask : Slow();
+        }
+
+        public Task<string> TaskOfStringOp(bool sync)
+        {
+            static async Task<string> Slow()
+            {
+                await Task.Yield();
+                return "success";
+            }
+
+            return sync ? Task.FromResult("success") : Slow();
+        }
+
+        public ValueTask ValueTaskOp(bool sync)
+        {
+            static async Task Slow()
+            {
+                await Task.Yield();
+            }
+
+            return sync ? default : new(Slow());
+        }
+
+        public ValueTask<string> ValueTaskOfStringOp(bool sync)
+        {
+            static async Task<string> Slow()
+            {
+                await Task.Yield();
+                return "success";
+            }
+
+            return sync ? new("success") : new(Slow());
+        }
+
+        int Add(int? value, Optional<TaskEntityContext> context, Optional<TaskEntityOperation> operation)
+        {
+            if (context.IsPresent)
+            {
+                context.Value.Should().NotBeNull();
+            }
+
+            if (operation.IsPresent)
+            {
+                operation.Value.Should().NotBeNull();
+            }
+
+            if (!value.HasValue && operation.TryGet(out TaskEntityOperation op))
+            {
+                value = (int)op.GetInput(typeof(int))!;
+            }
+
+            value.HasValue.Should().BeTrue();
+            return this.Value += value!.Value;
+        }
+
+        int Get(Optional<TaskEntityContext> context)
+        {
+            if (context.IsPresent)
+            {
+                context.Value.Should().NotBeNull();
+            }
+
+            return this.Value;
+        }
+    }
+}

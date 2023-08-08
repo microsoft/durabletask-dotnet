d9a125f5a8a70a4b9157237e64eb09d9d3fcff08
Jacob Viau <javia@microsoft.com>
2023-08-08 16:14:33 -0700

    EntityQuery.Name -> EntityIdStartsWith

--- src/Client/Core/Entities/EntityQuery.cs
+++ src/Client/Core/Entities/EntityQuery.cs
@@ -8,23 +8,50 @@ namespace Microsoft.DurableTask.Client.Entities;
 /// </summary>
 public record EntityQuery
 {
+    string? instanceIdStartsWith;
+
     /// <summary>
     /// The default page size.
     /// </summary>
     public const int DefaultPageSize = 100;
 
     /// <summary>
-    /// Gets the optional name of the entity to query for.
+    /// Gets the optional starts-with expression for the entity instance ID.
     /// </summary>
-    public string? Name { get; init; }
+    /// <remarks>
+    /// <para>
+    /// Entity IDs are expressed as <c>"@{name}@{key}"</c>. The starting "@" may be included or left out, the results will be
+    /// the same. Including the separator "@" between name or key will have an affect.
+    /// </para>
+    /// <para>
+    /// To query for an exact entity name, include the separator "@". e.g.: <c>"exactNameMatch@"</c>.
+    /// </para>
+    /// <para>
+    /// To query for an entity name starts with, leave out the separator "@". e.g.: <c>"namePrefixMatch"</c>.
+    /// </para>
+    /// <para>
+    /// To query for an entity name match <b>and</b> a key prefix, include name match, the separator "@", and finally
+    /// the key prefix. e.g. <c>"exactNameMatch@keyPrefixMatch"</c>.
+    /// </para>
+    /// </remarks>
+    public string? InstanceIdStartsWith
+    {
+        get => this.instanceIdStartsWith;
+        init
+        {
+            // prefix '@' if filter value provided and not already prefixed with '@'.
+            this.instanceIdStartsWith = value?.Length > 0 && value[0] != '@'
+                ? $"@{value}" : value;
+        }
+    }
 
     /// <summary>
-    /// Gets entity instances which was last modified after the provided time.
+    /// Gets entity instances which were last modified after the provided time.
     /// </summary>
     public DateTimeOffset? LastModifiedFrom { get; init; }
 
     /// <summary>
-    /// Gets entity instances which was last modified before the provided time.
+    /// Gets entity instances which were last modified before the provided time.
     /// </summary>
     public DateTimeOffset? LastModifiedTo { get; init; }
 

7d415d8f2951918edc313829cb81ba994d4034ea
Jacob Viau <javia@microsoft.com>
2023-10-11 17:22:09 -0700

    Add write-only serializer for EntityMetadata

---
+++ test/Client/Core.Tests/Entities/EntityMetadataTests.cs
@@ -0,0 +1,130 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+using Microsoft.DurableTask.Entities;
+
+namespace Microsoft.DurableTask.Client.Entities.Tests;
+
+public class EntityMetadataTests
+{
+    readonly EntityInstanceId id = new("test", Random.Shared.Next(0, 100).ToString());
+
+    [Fact]
+    public void GetState_NotIncluded_Throws()
+    {
+        EntityMetadata<int> metadata = new(this.id);
+        Func<int> act = () => metadata.State;
+        act.Should().ThrowExactly<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void GetState_Included_DoesNotThrow()
+    {
+        int state = Random.Shared.Next(0, 100);
+        EntityMetadata<int> metadata = new(this.id, state);
+        metadata.State.Should().Be(state);
+    }
+
+    [Fact]
+    public void Serialize_StateNotIncluded()
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        string lockedBy = Guid.NewGuid().ToString("N");
+        EntityMetadata<int> metadata = new(this.id)
+        {
+            LastModifiedTime = now,
+            BacklogQueueSize = 10,
+            LockedBy = lockedBy,
+        };
+
+        string json = JsonSerializer.Serialize(metadata);
+        json.Should().Be($"{{\"Id\":\"{this.id}\",\"LastModifiedTime\":\"{now:O}\",\"BacklogQueueSize\":10,\"LockedBy\""
+            + $":\"{lockedBy}\"}}");
+    }
+
+    [Fact]
+    public void Serialize_StateIncluded_Int()
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        string lockedBy = Guid.NewGuid().ToString("N");
+        int state = Random.Shared.Next(0, 100);
+        EntityMetadata<int> metadata = new(this.id, state)
+        {
+            LastModifiedTime = now,
+            BacklogQueueSize = 10,
+            LockedBy = lockedBy,
+        };
+
+        string json = JsonSerializer.Serialize(metadata);
+        json.Should().Be($"{{\"Id\":\"{this.id}\",\"LastModifiedTime\":\"{now:O}\",\"BacklogQueueSize\":10,\"LockedBy\""
+            + $":\"{lockedBy}\",\"State\":{state}}}");
+    }
+
+    [Fact]
+    public void Serialize_StateIncluded_Object()
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        string lockedBy = Guid.NewGuid().ToString("N");
+        State state = State.GetRandom();
+        EntityMetadata<State> metadata = new(this.id, state)
+        {
+            LastModifiedTime = now,
+            BacklogQueueSize = 10,
+            LockedBy = lockedBy,
+        };
+
+        string json = JsonSerializer.Serialize(metadata);
+        json.Should().Be($"{{\"Id\":\"{this.id}\",\"LastModifiedTime\":\"{now:O}\",\"BacklogQueueSize\":10,\"LockedBy\""
+            + $":\"{lockedBy}\",\"State\":{{\"Number\":{state.Number}}}}}");
+    }
+    
+    [Fact]
+    public void Serialize_StateIncluded_Object2()
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        State state = State.GetRandom();
+        EntityMetadata<State> metadata = new(this.id, state)
+        {
+            LastModifiedTime = now,
+        };
+
+        string json = JsonSerializer.Serialize(metadata);
+        json.Should().Be($"{{\"Id\":\"{this.id}\",\"LastModifiedTime\":\"{now:O}\",\"State\":"
+            + $"{{\"Number\":{state.Number}}}}}");
+    }
+
+    [Fact]
+    public void Serialize_StateNotIncluded_NonGeneric()
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        State state = State.GetRandom();
+        EntityMetadata metadata = new(this.id)
+        {
+            LastModifiedTime = now,
+        };
+
+        string json = JsonSerializer.Serialize(metadata);
+        json.Should().Be($"{{\"Id\":\"{this.id}\",\"LastModifiedTime\":\"{now:O}\"}}");
+    }
+
+
+    [Fact]
+    public void Serialize_StateIncluded_NonGeneric()
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+        State state = State.GetRandom();
+        EntityMetadata metadata = new(this.id, SerializedData.Create(state))
+        {
+            LastModifiedTime = now,
+        };
+
+        string json = JsonSerializer.Serialize(metadata);
+        json.Should().Be($@"{{""Id"":""{this.id}"",""LastModifiedTime"":""{now:O}"",""State"":""{{\u0022Number\u0022:{state.Number}}}""}}");
+    }
+
+    record class State(int Number)
+    {
+        public static State GetRandom() => new(Random.Shared.Next(0, 100));
+    }
+}

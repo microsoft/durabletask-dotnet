3a375776d7b5244fcbe9adb97927e866b28ef83f
David Justo <david.justo.1996@gmail.com>
2023-11-12 13:21:03 -0800

    implement Read method

--- test/Client/Core.Tests/Entities/EntityMetadataTests.cs
+++ test/Client/Core.Tests/Entities/EntityMetadataTests.cs
@@ -14,6 +14,11 @@ public class EntityMetadataTests
     // create customize convert class
     public class DateTimeConverter : JsonConverter<DateTime>
     {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.Parse(reader.GetString());
+        }
+
         public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
         {
             writer.WriteStringValue(value.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss'.'fffffffzzz"));

14b94ebdd51119d92110b9547ee221cf3568a565
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2025-01-10 15:24:38 -0800

    sample
    sample fix

---
+++ samples/portable-sdk/dotnet/AspNetWebApp/Orchestrations/HelloCities.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+
+namespace AspNetWebApp.Scenarios;
+
+[DurableTask]
+class HelloCities : TaskOrchestrator<string, List<string>>
+{
+    public override async Task<List<string>> RunAsync(TaskOrchestrationContext context, string input)
+    {
+        List<string> results =
+        [
+            await context.CallSayHelloAsync("Seattle"),
+            await context.CallSayHelloAsync("Amsterdam"),
+            await context.CallSayHelloAsync("Hyderabad"),
+            await context.CallSayHelloAsync("Shanghai"),
+            await context.CallSayHelloAsync("Tokyo"),
+        ];
+        return results;
+    }
+}
+
+[DurableTask]
+class SayHello : TaskActivity<string, string>
+{
+    public override Task<string> RunAsync(TaskActivityContext context, string cityName)
+    {
+        return Task.FromResult($"Hello, {cityName}!");
+    }
+}
---
+++ samples/portable-sdk/dotnet/AspNetWebApp/Utils.cs
@@ -0,0 +1,38 @@
+﻿namespace AspNetWebApp;
+
+static class Utils
+{
+    public static async Task ParallelForEachAsync<T>(this IEnumerable<T> items, int maxConcurrency, Func<T, Task> action)
+    {
+        List<Task> tasks;
+        if (items is ICollection<T> itemCollection)
+        {
+            tasks = new List<Task>(itemCollection.Count);
+        }
+        else
+        {
+            tasks = [];
+        }
+
+        using SemaphoreSlim semaphore = new(maxConcurrency);
+        foreach (T item in items)
+        {
+            tasks.Add(InvokeThrottledAction(item, action, semaphore));
+        }
+
+        await Task.WhenAll(tasks);
+    }
+
+    static async Task InvokeThrottledAction<T>(T item, Func<T, Task> action, SemaphoreSlim semaphore)
+    {
+        await semaphore.WaitAsync();
+        try
+        {
+            await action(item);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+}

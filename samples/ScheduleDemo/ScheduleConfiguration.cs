9987b15ffbbb51faa33d33dc02ea4b59ad40e86a
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2025-02-18 22:54:06 -0800

    save

--- samples/ScheduleDemo/ScheduleConfiguration.cs
+++ samples/ScheduleDemo/ScheduleConfiguration.cs
@@ -1,58 +1,58 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
+// // Copyright (c) Microsoft Corporation.
+// // Licensed under the MIT License.
 
-class ScheduleConfiguration
-{
-    public ScheduleConfiguration(string orchestrationName, string scheduleId)
-    {
-        this.OrchestrationName = orchestrationName;
-        this.ScheduleId = scheduleId ?? Guid.NewGuid().ToString("N");
-        this.Version++;
-    }
+// class ScheduleConfiguration
+// {
+//     public ScheduleConfiguration(string orchestrationName, string scheduleId)
+//     {
+//         this.OrchestrationName = orchestrationName;
+//         this.ScheduleId = scheduleId ?? Guid.NewGuid().ToString("N");
+//         this.Version++;
+//     }
 
-    public string OrchestrationName { get; set; }
+//     public string OrchestrationName { get; set; }
 
-    public string ScheduleId { get; set; }
+//     public string ScheduleId { get; set; }
 
-    public string? OrchestrationInput { get; set; }
+//     public string? OrchestrationInput { get; set; }
 
-    public string? OrchestrationInstanceId { get; set; } = Guid.NewGuid().ToString("N");
+//     public string? OrchestrationInstanceId { get; set; } = Guid.NewGuid().ToString("N");
 
-    public DateTimeOffset? StartAt { get; set; }
+//     public DateTimeOffset? StartAt { get; set; }
 
-    public DateTimeOffset? EndAt { get; set; }
+//     public DateTimeOffset? EndAt { get; set; }
 
-    TimeSpan? interval;
+//     TimeSpan? interval;
 
-    public TimeSpan? Interval
-    {
-        get => this.interval;
-        set
-        {
-            if (!value.HasValue)
-            {
-                return;
-            }
+//     public TimeSpan? Interval
+//     {
+//         get => this.interval;
+//         set
+//         {
+//             if (!value.HasValue)
+//             {
+//                 return;
+//             }
 
-            if (value.Value <= TimeSpan.Zero)
-            {
-                throw new ArgumentException("Interval must be positive", nameof(value));
-            }
+//             if (value.Value <= TimeSpan.Zero)
+//             {
+//                 throw new ArgumentException("Interval must be positive", nameof(value));
+//             }
 
-            if (value.Value.TotalSeconds < 1)
-            {
-                throw new ArgumentException("Interval must be at least 1 second", nameof(value));
-            }
+//             if (value.Value.TotalSeconds < 1)
+//             {
+//                 throw new ArgumentException("Interval must be at least 1 second", nameof(value));
+//             }
 
-            this.interval = value;
-        }
-    }
+//             this.interval = value;
+//         }
+//     }
 
-    public string? CronExpression { get; set; }
+//     public string? CronExpression { get; set; }
 
-    public int MaxOccurrence { get; set; }
+//     public int MaxOccurrence { get; set; }
 
-    public bool? StartImmediatelyIfLate { get; set; }
+//     public bool? StartImmediatelyIfLate { get; set; }
 
-    internal int Version { get; set; } // Tracking schedule config version
-}
+//     internal int Version { get; set; } // Tracking schedule config version
+// }

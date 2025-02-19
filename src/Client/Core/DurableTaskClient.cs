93ee63213b568778f34b71ba1aac7fab946b34e5
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2025-02-18 22:33:46 -0800

    save

--- src/Client/Core/DurableTaskClient.cs
+++ src/Client/Core/DurableTaskClient.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.ComponentModel;
-using DurableTask.Abstractions.Entities.Schedule;
 using Microsoft.DurableTask.Client.Entities;
 using Microsoft.DurableTask.Internal;
 

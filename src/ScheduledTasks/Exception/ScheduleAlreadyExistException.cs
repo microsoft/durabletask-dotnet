7b413757399207d4a6b880c088b463cbf3ada30c
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2025-02-25 08:43:19 -0800

    excep

--- src/ScheduledTasks/Exception/ScheduleAlreadyExistException.cs
+++ src/ScheduledTasks/Exception/ScheduleAlreadyExistException.cs
@@ -6,24 +6,24 @@ namespace Microsoft.DurableTask.ScheduledTasks;
 /// <summary>
 /// Exception thrown when attempting to create a schedule with an ID that already exists.
 /// </summary>
-public class ScheduleAlreadyExistException : InvalidOperationException
+public class ScheduleAlreadyExistsException : InvalidOperationException
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="ScheduleAlreadyExistException"/> class.
+    /// Initializes a new instance of the <see cref="ScheduleAlreadyExistsException"/> class.
     /// </summary>
     /// <param name="scheduleId">The ID of the schedule that already exists.</param>
-    public ScheduleAlreadyExistException(string scheduleId)
+    public ScheduleAlreadyExistsException(string scheduleId)
         : base($"A schedule with ID '{scheduleId}' already exists.")
     {
         this.ScheduleId = scheduleId;
     }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="ScheduleAlreadyExistException"/> class.
+    /// Initializes a new instance of the <see cref="ScheduleAlreadyExistsException"/> class.
     /// </summary>
     /// <param name="scheduleId">The ID of the schedule that already exists.</param>
     /// <param name="innerException">The exception that is the cause of the current exception.</param>
-    public ScheduleAlreadyExistException(string scheduleId, Exception innerException)
+    public ScheduleAlreadyExistsException(string scheduleId, Exception innerException)
         : base($"A schedule with ID '{scheduleId}' already exists.", innerException)
     {
         this.ScheduleId = scheduleId;

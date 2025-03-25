64503f731e251f8b78545d9931597f0b06ab795d
peterstone2017 <12449837+YunchuWang@users.noreply.github.com>
2025-03-25 09:15:24 -0700

    Refactor PurgeResult

--- src/Client/Core/PurgeResult.cs
+++ src/Client/Core/PurgeResult.cs
@@ -12,11 +12,23 @@ public class PurgeResult
     /// Initializes a new instance of the <see cref="PurgeResult" /> class.
     /// </summary>
     /// <param name="count">The count of instances purged.</param>
-    /// <param name="isComplete">A value indicating whether the purge operation is complete. Default is null.</param>
-    public PurgeResult(int count, bool? isComplete = null)
+    public PurgeResult(int count)
     {
         Check.Argument(count >= 0, nameof(count), "Count must be non-negative");
         this.PurgedInstanceCount = count;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PurgeResult" /> class.
+    /// </summary>
+    /// <param name="count">The number of instances deleted.</param>
+    /// <param name="isComplete">A value indicating whether the purge operation is complete. 
+    /// If true, the purge operation is complete. All instances were purged.
+    /// If false, not all instances were purged. Please purge again.
+    /// If null, default to legacy purge behavior. All instances were purged.</param>
+    public PurgeResult(int count, bool? isComplete)
+        : this(count)
+    {
         this.IsComplete = isComplete;
     }
 

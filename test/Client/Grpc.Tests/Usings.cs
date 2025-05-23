387307c9f34aca27a4afade1dbbfae332c0b7dbe
Whit Waldo <whit.waldo@innovian.net>
2025-05-22 19:36:21 -0500

    Changing how versions are applied to packages (#12)
    * Adding minver for versioning this based on Git tags

--- test/Client/Grpc.Tests/Usings.cs
+++ test/Client/Grpc.Tests/Usings.cs
@@ -5,11 +5,4 @@ global using FluentAssertions;
 global using Moq;
 global using Xunit;
 
-#if NET6_0_OR_GREATER
 global using Grpc.Net.Client;
-#endif
-
-#if NETFRAMEWORK
-global using Grpc.Core;
-global using GrpcChannel = Grpc.Core.Channel;
-#endif

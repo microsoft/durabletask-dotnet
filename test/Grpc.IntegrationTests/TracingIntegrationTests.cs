5b44cef11d2fd7e82692d91e988630ac48cf63cc
Phillip Hoff <phillip@orst.edu>
2025-06-11 16:44:46 -0700

    Sketch basic tracing test.

--- test/Grpc.IntegrationTests/TracingIntegrationTests.cs
+++ test/Grpc.IntegrationTests/TracingIntegrationTests.cs
@@ -37,31 +37,36 @@ public class TracingIntegrationTests : IntegrationTestBase
     static readonly ActivitySource TestActivitySource = new(TestActivitySourceName);
 
     [Fact]
-    public async Task Orchestration_Traces()
+    public async Task MultiTaskOrchestration()
     {
         var activities = new List<Activity>();
 
         using var listener = CreateListener(ActivitySourceNames, activities);
 
-        string orchestratorName = nameof(Orchestration_Traces);
+        string orchestratorName = nameof(MultiTaskOrchestration);
 
         await using HostTestLifetime server = await this.StartWorkerAsync(b =>
         {
             b.AddTasks(tasks => tasks
-                .AddOrchestratorFunc<string, int>(
-                    orchestratorName, (ctx, input) => PageableOrchestrationAsync(ctx, input))
-                .AddActivityFunc<PageRequest, Page<string>?>(
-                    nameof(PageableActivityAsync), (_, input) => PageableActivityAsync(input)));
+                .AddOrchestratorFunc<bool, bool>(
+                    orchestratorName,
+                    async (ctx, input) =>
+                    {
+                        await ctx.CallActivityAsync(nameof(TestActivityAsync), true);
+                        await ctx.CallActivityAsync(nameof(TestActivityAsync), true);
+
+                        return true;
+                    })
+                .AddActivityFunc<bool, bool>(nameof(TestActivityAsync), (_, input) => TestActivityAsync(input)));
         });
 
         string instanceId;
 
-        using (var activity = TestActivitySource.StartActivity("Test", ActivityKind.Client))
+        using (var activity = TestActivitySource.StartActivity("Test"))
         {
-            instanceId = await server.Client.ScheduleNewOrchestrationInstanceAsync(
-                orchestratorName, input: string.Empty);
-            OrchestrationMetadata metadata = await server.Client.WaitForInstanceCompletionAsync(
-                instanceId, getInputsAndOutputs: true, this.TimeoutToken);
+            instanceId = await server.Client.ScheduleNewOrchestrationInstanceAsync(orchestratorName, input: true, cancellation: this.TimeoutToken);
+
+            OrchestrationMetadata metadata = await server.Client.WaitForInstanceCompletionAsync(instanceId, getInputsAndOutputs: true, this.TimeoutToken);
         }
 
         var testActivity = activities.Single(a => a.Source == TestActivitySource && a.OperationName == "Test");
@@ -76,15 +81,16 @@ public class TracingIntegrationTests : IntegrationTestBase
 
         var orchestrationActivities = activities.Where(a => a.Source.Name == CoreActivitySourceName && a.OperationName == "orchestration:Orchestration_Traces").ToList();
 
-        // The orchestration activities should be the same "logical" activity.
+        orchestrationActivities.Should().HaveCountGreaterThan(0);
+
+        // The orchestration activities should be the same "logical" orchestration activity.
         orchestrationActivities.Select(a => a.StartTimeUtc).Distinct().Should().HaveCount(1);
         orchestrationActivities.Select(a => a.Id).Distinct().Should().HaveCount(1);
         orchestrationActivities.Select(a => a.SpanId).Distinct().Should().HaveCount(1);
 
         // The orchestration activities should be parented to the create activity.
         orchestrationActivities
-            .Should().HaveCountGreaterThan(0)
-            .And.AllSatisfy(a =>
+            .Should().AllSatisfy(a =>
             {
                 a.ParentId.Should().Be(createActivity.Id);
                 a.ParentSpanId.Should().Be(createActivity.SpanId);
@@ -96,66 +102,67 @@ public class TracingIntegrationTests : IntegrationTestBase
         
         var orchestrationActivity = orchestrationActivities.First();
 
-        var activityActivities = activities.Where(a => a.Source.Name == CoreActivitySourceName && a.OperationName == "activity:PageableActivityAsync").ToList();
+        var clientActivityActivities = activities.Where(a => a.Kind == ActivityKind.Client && a.Source.Name == CoreActivitySourceName && a.OperationName == "activity:TestActivityAsync").ToList();
 
-        activityActivities
-            .Should().HaveCountGreaterThan(0)
+        // The "client" (i.e. scheduled) task activities should be parented to the orchestration activity.
+        clientActivityActivities
+            .Should().HaveCount(2)
             .And.AllSatisfy(a =>
             {
                 a.ParentId.Should().Be(orchestrationActivity.Id);
                 a.ParentSpanId.Should().Be(orchestrationActivity.SpanId);
                 
                 a.TagObjects.Should().ContainKey("durabletask.task.instance_id").WhoseValue.Should().Be(instanceId);
-                a.TagObjects.Should().ContainKey("durabletask.task.name").WhoseValue.Should().Be(nameof(PageableActivityAsync));
+                a.TagObjects.Should().ContainKey("durabletask.task.name").WhoseValue.Should().Be(nameof(TestActivityAsync));
                 a.TagObjects.Should().ContainKey("durabletask.type").WhoseValue.Should().Be("activity");
+                a.TagObjects.Should().ContainKey("durabletask.task.task_id").WhoseValue.Should().NotBeNull();
             });
 
-        activityActivities
+        clientActivityActivities
             .Zip(Enumerable.Range(0, Int32.MaxValue))
             .Should().AllSatisfy(indexed =>
             {
                 indexed.First.TagObjects.Should().ContainKey("durabletask.task.task_id").WhoseValue.Should().Be(indexed.Second);
             });
 
-        var activityExecutionActivities = activities.Where(a => a.Source.Name == TestActivitySourceName && a.OperationName == nameof(PageableActivityAsync)).ToList();
+        var serverActivityActivities = activities.Where(a => a.Kind == ActivityKind.Server && a.Source.Name == CoreActivitySourceName && a.OperationName == "activity:TestActivityAsync").ToList();
 
-        activityExecutionActivities
-            .Should().HaveCount(activityActivities.Count)
+        // The "server" (i.e. executed) task activities should be parented to the client activity activities.
+        serverActivityActivities
+            .Should().HaveCount(clientActivityActivities.Count)
             .And.AllSatisfy(a =>
             {
-                a.ParentId.Should().BeOneOf(activityActivities.Select(aa => aa.Id));
-                a.ParentSpanId.ToString().Should().BeOneOf(activityActivities.Select(aa => aa.SpanId.ToString()));
+                a.ParentId.Should().BeOneOf(clientActivityActivities.Select(aa => aa.Id));
+                a.ParentSpanId.ToString().Should().BeOneOf(clientActivityActivities.Select(aa => aa.SpanId.ToString()));
+            });
+
+        var activityExecutionActivities = activities.Where(a => a.Source.Name == TestActivitySourceName && a.OperationName == nameof(TestActivityAsync)).ToList();
+
+        // The activity execution activities should be parented to the server activity activities.
+        activityExecutionActivities
+            .Should().HaveCount(serverActivityActivities.Count)
+            .And.AllSatisfy(a =>
+            {
+                a.ParentId.Should().BeOneOf(serverActivityActivities.Select(aa => aa.Id));
+                a.ParentSpanId.ToString().Should().BeOneOf(serverActivityActivities.Select(aa => aa.SpanId.ToString()));
             });
     }
 
-    static Task<Page<string>?> PageableActivityAsync(PageRequest? input)
+    static Task<bool> TestActivityAsync(bool shouldSucceed)
     {
         using var activity = TestActivitySource.StartActivity();
 
-        int pageSize = input?.PageSize ?? 3;
-        Page<string> CreatePage(string? next)
-            => new (Enumerable.Range(0, pageSize).Select(x => $"item_{x}").ToList(), next);
-        Page<string>? page = input?.Continuation switch
+        if (shouldSucceed)
         {
-            null => CreatePage("1"),
-            "1" => CreatePage("2"),
-            "2" => CreatePage(null),
-            _ => null,
-        };
+            activity?.SetStatus(ActivityStatusCode.Ok);
 
-        return Task.FromResult(page);
-    }
-
-    static async Task<int> PageableOrchestrationAsync(TaskOrchestrationContext context, string? input)
-    {
-        AsyncPageable<string> pageable = Pageable.Create((continuation, _, _) =>
+            return Task.FromResult(true);
+        }
+        else
         {
-            return context.CallActivityAsync<Page<string>?>(
-                nameof(PageableActivityAsync), new PageRequest(continuation))!;
-        });
+            activity?.SetStatus(ActivityStatusCode.Error);
 
-        return await pageable.CountAsync();
+            return Task.FromResult(false);
+        }
     }
-
-    record PageRequest(string? Continuation, int? PageSize = null);
 }

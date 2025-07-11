0c5d210c9890700df4305ed420eee606724a7d97
Sophia Tevosyan <stevosyan@microsoft.com>
2025-07-11 13:10:13 -0700

    adding tests that are not complete for now

---
+++ test/Abstractions.Tests/TaskOptionsTests.cs
@@ -0,0 +1,42 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.DurableTask.Tests;
+
+public class TaskOptionsTests
+{
+    [Fact]
+    public void Empty_Ctors_Okay()
+    {
+        TaskOptions options = new();
+        options.Retry.Should().BeNull();
+        options.Tags.Should().BeNull();
+
+        SubOrchestrationOptions subOptions = new();
+        subOptions.Retry.Should().BeNull();
+        subOptions.Tags.Should().BeNull();
+        subOptions.InstanceId.Should().BeNull();
+
+        StartOrchestrationOptions startOptions = new();
+        startOptions.Version.Should().BeNull();
+        startOptions.InstanceId.Should().BeNull();
+        startOptions.StartAt.Should().BeNull();
+        startOptions.Tags.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void SubOrchestrationOptions_InstanceId_Overriden()
+    {
+        string instanceId = Guid.NewGuid().ToString();
+        SubOrchestrationOptions subOptions = new(new TaskOptions(), instanceId);
+        subOptions.Retry.Should().BeNull();
+        subOptions.Tags.Should().BeNull();
+        subOptions.InstanceId.Should().BeNull();
+
+        StartOrchestrationOptions startOptions = new();
+        startOptions.Version.Should().BeNull();
+        startOptions.InstanceId.Should().BeNull();
+        startOptions.StartAt.Should().BeNull();
+        startOptions.Tags.Should().BeEmpty();
+    }
+}

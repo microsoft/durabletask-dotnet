b5d84812f3f1439416d43434c966b103a5bfe15c
Sophia Tevosyan <stevosyan@microsoft.com>
2025-08-01 14:47:39 -0700

    added comments

--- src/Worker/Grpc/ExtendedSessionsCache.cs
+++ src/Worker/Grpc/ExtendedSessionsCache.cs
@@ -5,15 +5,27 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace Microsoft.DurableTask.Worker.Grpc;
 
+/// <summary>
+/// A cache for extended sessions that wraps an <see cref="IMemoryCache"/> instance.
+/// Responsible for holding <see cref="ExtendedSessionState"/> for orchestrations that are running within extended sessions.
+/// </summary>
 public class ExtendedSessionsCache
 {
-    private IMemoryCache? extendedSessions;
+    IMemoryCache? extendedSessions;
 
-    internal IMemoryCache GetOrInitializeCache(double extendedSessionIdleTimeoutInSeconds)
+    /// <summary>
+    /// Gets the cache for extended sessions if it has already been initialized, or otherwise initializes it with the given expiration scan frequency.
+    /// </summary>
+    /// <param name="expirationScanFrequencyInSeconds">
+    /// The expiration scan frequency of the cache, in seconds. T
+    /// This specifies how often the cache checks for stale items, and evicts them.
+    /// </param>
+    /// <returns>The IMemoryCache that holds the cached <see cref="ExtendedSessionState"/>.</returns>
+    internal IMemoryCache GetOrInitializeCache(double expirationScanFrequencyInSeconds)
     {
         this.extendedSessions ??= new MemoryCache(new MemoryCacheOptions
         {
-            ExpirationScanFrequency = TimeSpan.FromSeconds(extendedSessionIdleTimeoutInSeconds),
+            ExpirationScanFrequency = TimeSpan.FromSeconds(expirationScanFrequencyInSeconds),
         });
 
         return this.extendedSessions;

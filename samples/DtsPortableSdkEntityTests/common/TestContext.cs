38d54069fe8734a3be696d7abb473989fe092164
sebastianburckhardt <sburckha@microsoft.com>
2025-09-02 14:24:19 -0700

    add standalone app that uses the portable SDK for testing entities.

---
+++ samples/DtsPortableSdkEntityTests/common/TestContext.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.DurableTask.Client;
+using Microsoft.DurableTask.Client.Entities;
+using Microsoft.DurableTask.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace DtsPortableSdkEntityTests;
+
+internal class TestContext
+{
+    public TestContext(DurableTaskClient client, ILogger logger, CancellationToken cancellationToken)
+    {
+        this.Client = client;
+        this.Logger = logger;
+        this.CancellationToken = cancellationToken;
+    }
+
+    public DurableTaskClient Client { get; }
+
+    public ILogger Logger { get; }
+
+    public CancellationToken CancellationToken { get; set; }
+
+    public bool BackendSupportsImplicitEntityDeletion { get; set; } = true;  // false for Azure Storage, true for Netherite, MSSQL, and DTS
+}

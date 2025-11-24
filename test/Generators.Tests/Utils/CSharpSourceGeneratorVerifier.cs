53f3c96c191b73d8aab1d753ecae2c81e918f2e4
Tomer Rosenthal <torosent@microsoft.com>
2025-11-23 21:17:57 -0800

    Update project to support .NET 6.0 alongside .NET 8.0 and .NET 10, and adjust build configurations

--- test/Generators.Tests/Utils/CSharpSourceGeneratorVerifier.cs
+++ test/Generators.Tests/Utils/CSharpSourceGeneratorVerifier.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Immutable;
+using System.IO;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Testing;
@@ -16,11 +18,7 @@ public static class CSharpSourceGeneratorVerifier<TSourceGenerator> where TSourc
     {
         public Test()
         {
-            // See https://www.nuget.org/packages/Microsoft.NETCore.App.Ref/8.0.22
-            this.ReferenceAssemblies = new ReferenceAssemblies(
-                targetFramework: "net8.0",
-                referenceAssemblyPackage: new PackageIdentity("Microsoft.NETCore.App.Ref", "8.0.22"),
-                referenceAssemblyPath: Path.Combine("ref", "net8.0"));
+            this.ReferenceAssemblies = CreateReferenceAssembliesForTargetFramework();
         }
 
         public LanguageVersion LanguageVersion { get; set; } = LanguageVersion.CSharp9;
@@ -47,6 +45,28 @@ public static class CSharpSourceGeneratorVerifier<TSourceGenerator> where TSourc
         protected override ParseOptions CreateParseOptions()
         {
             return ((CSharpParseOptions)base.CreateParseOptions()).WithLanguageVersion(this.LanguageVersion);
+        }
+
+        static ReferenceAssemblies CreateReferenceAssembliesForTargetFramework()
+        {
+#if NET10_0_OR_GREATER
+            return new ReferenceAssemblies(
+                targetFramework: "net10.0",
+                referenceAssemblyPackage: new PackageIdentity("Microsoft.NETCore.App.Ref", "10.0.0"),
+                referenceAssemblyPath: Path.Combine("ref", "net10.0"));
+#elif NET8_0_OR_GREATER
+            return new ReferenceAssemblies(
+                targetFramework: "net8.0",
+                referenceAssemblyPackage: new PackageIdentity("Microsoft.NETCore.App.Ref", "8.0.22"),
+                referenceAssemblyPath: Path.Combine("ref", "net8.0"));
+#elif NET6_0_OR_GREATER
+            return new ReferenceAssemblies(
+                targetFramework: "net6.0",
+                referenceAssemblyPackage: new PackageIdentity("Microsoft.NETCore.App.Ref", "6.0.36"),
+                referenceAssemblyPath: Path.Combine("ref", "net6.0"));
+#else
+            throw new NotSupportedException("Unsupported target framework for generator tests.");
+#endif
         }
     }
 }

278f025146ba9a14d9d01386247bb685b476719a
wangbill <gggwang1993@gmail.com>
2026-06-11 09:10:47 -0700

    Include on-demand sandbox proto in refresh script

--- src/Grpc/refresh-protos.ps1
+++ src/Grpc/refresh-protos.ps1
@@ -18,7 +18,8 @@ $commitId = $commitDetails.sha
 
 # These are the proto files we need to download from the durabletask-protobuf repository.
 $protoFileNames = @(
-    "orchestrator_service.proto"
+    "orchestrator_service.proto",
+    "on_demand_sandbox_activities_service.proto"
 )
 
 # Download each proto file to the local directory using the above commit ID

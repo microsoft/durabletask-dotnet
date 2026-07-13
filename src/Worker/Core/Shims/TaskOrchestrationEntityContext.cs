9fd731ad8447084b82c56c6a2d6796e26e39bdb5
Sophia Tevosyan <stevosyan@microsoft.com>
2026-07-13 10:41:51 -0700

    added logic to send lock release messages to entities when lock acquisition fails

--- src/Worker/Core/Shims/TaskOrchestrationEntityContext.cs
+++ src/Worker/Core/Shims/TaskOrchestrationEntityContext.cs
@@ -82,6 +82,11 @@ sealed partial class TaskOrchestrationContextWrapper
 
             if (result.IsError)
             {
+                foreach (EntityMessageEvent releaseMessage in this.EntityContext.EmitLockReleaseMessages())
+                {
+                    this.wrapper.innerContext.SendEvent(releaseMessage.TargetInstance, releaseMessage.EventName, releaseMessage.AsRawInput());
+                }
+
                 this.EntityContext.AbandonAcquire();
                 throw new EntityLockAcquisitionFailedException(entityIds, ConvertFailureDetails(result.FailureDetails!));
             }
